dda058249479606832182fd92a1ea9ad1da02a5c
NghiemHieu7295 <nmh7295@gmail.com>
2020-01-30 16:55:16 +0700

    Xong chuong 4

--- Ex04Cart/Ex04Cart/Cart.aspx.cs
+++ Ex04Cart/Ex04Cart/Cart.aspx.cs
@@ -9,9 +9,52 @@ namespace Ex04Cart
 {
     public partial class Cart : System.Web.UI.Page
     {
+        private CartItemList itemList;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            itemList = CartItemList.GetCart();
+            if(!IsPostBack)
+                this.DisplayCart();
+        }
 
+        private void DisplayCart()
+        {
+            lstCart.Items.Clear();
+            if (itemList.Count > 0)
+            {
+                for (int i = 0; i < itemList.Count; i++)
+                {
+                    lstCart.Items.Add(itemList[i].Display());
+                }
+            }
+        }
+
+        protected void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (itemList.Count > 0)
+            {
+                if (lstCart.SelectedIndex > -1)
+                {
+                    itemList.RemoveAt(lstCart.SelectedIndex);
+                    this.DisplayCart();
+                }
+                else
+                {
+                    lblMessage.Text = "Please select a product to remove";
+                }
+            }
+        }
+
+        protected void btnEmpty_Click(object sender, EventArgs e)
+        {
+            itemList.Clear();
+            lstCart.Items.Clear();
+        }
+
+        protected void btnCheckOut_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Sorry, this fuction hasn't been implemented yet.";
         }
     }
 }
--- Ex04Cart/Ex04Cart/Order.aspx.cs
+++ Ex04Cart/Ex04Cart/Order.aspx.cs
@@ -4,14 +4,62 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace Ex04Cart
 {
     public partial class Order : System.Web.UI.Page
     {
+        private Product selectedProduct;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                ddlProducts.DataBind();
 
+            selectedProduct = this.GetSelectedProduct();
+            lblName.Text = selectedProduct.Name;
+            lblShortDescription.Text = selectedProduct.ShortDescription;
+            lblLongDescription.Text = selectedProduct.LongDescription;
+            lblUnitPrice.Text = selectedProduct.UnitPrice.ToString("c") + " each";
+            imgProduct.ImageUrl = "Images/Products/" + selectedProduct.ImageFile;
+        }
+
+        private Product GetSelectedProduct()
+        {
+            DataView productsTable = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            productsTable.RowFilter = "ProductID = '" + ddlProducts.SelectedValue + "'";
+            DataRowView row = productsTable[0];
+
+            Product p = new Product();
+            p.ProductID = row["ProductID"].ToString();
+            p.Name = row["Name"].ToString();
+            p.ShortDescription = row["ShortDescription"].ToString();
+            p.LongDescription = row["LongDescription"].ToString();
+            p.UnitPrice = (decimal)row["UnitPrice"];
+            p.ImageFile = row["ImageFile"].ToString();
+
+            return p;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                CartItemList cart = CartItemList.GetCart();
+                CartItem item = cart[selectedProduct.ProductID];
+
+                if (item == null)
+                {
+                    cart.AddItem(selectedProduct, Int32.Parse(txtQuantity.Text));
+                }
+                else
+                {
+                    item.AddQuantity(Convert.ToInt32(txtQuantity.Text));
+                }
+
+                Response.Redirect("~/Cart.aspx");
+            }
         }
     }
 }
